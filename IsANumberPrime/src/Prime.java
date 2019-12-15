9e7cffcd1a10f1bd7bb36380cbe5a27ad73edb62
Bíró Botond <biro.botond20@gmail.com>
2019-12-15 12:34:18 +0100

    feat(IsANumberPrime): add solution for Is a number prime? kata

--- IsANumberPrime/src/Prime.java
+++ IsANumberPrime/src/Prime.java
@@ -1,7 +1,9 @@
 /**
- * Define a function that takes an integer argument and returns logical value true or false depending on if the integer is a prime.
+ * Define a function that takes an integer argument and returns logical value true or false
+ * depending on if the integer is a prime.
  *
- * Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+ * Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no
+ * positive divisors other than 1 and itself.
  * Example
  *
  * is_prime(1)  * false *
@@ -10,9 +12,10 @@
  *
  * Assumptions
  *  - You can assume you will be given an integer input.
- *  - You can not assume that the integer will be only positive.You may be given negative numbers
- *  - as well(or 0).
- * There are no fancy optimizations required,but still the most trivial solutions might time out.Try to find a solution which does not loop all the way up to n.
+ *  - You can not assume that the integer will be only positive. You may be given negative numbers
+ *    as well(or 0).
+ * There are no fancy optimizations required, but still the most trivial solutions might time out.
+ * Try to find a solution which does not loop all the way up to n.
  */
 
 public class Prime {
